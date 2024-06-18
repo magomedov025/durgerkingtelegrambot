2d7088a9ee33785322b54e8cb18daf2a4ede21be
magomedov025 <magomedov025@yahoo.com>
2024-06-18 19:30:27 +0300

    new commit

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const https = require('https')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -66,6 +68,29 @@ app.post('/create-payment', async (req, res) => {
 	}
 })
 
-app.listen(port, () => {
-	console.log(`Server is running on https://31.128.40.24:${port}`)
+// Чтение сертификатов SSL
+const privateKey = fs.readFileSync(
+	'/etc/letsencrypt/live/example.com/privkey.pem',
+	'utf8'
+)
+const certificate = fs.readFileSync(
+	'/etc/letsencrypt/live/example.com/cert.pem',
+	'utf8'
+)
+const ca = fs.readFileSync(
+	'/etc/letsencrypt/live/example.com/chain.pem',
+	'utf8'
+)
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca,
+}
+
+// Создание HTTPS сервера
+const httpsServer = https.createServer(credentials, app)
+
+httpsServer.listen(port, () => {
+	console.log(`HTTPS Server is running on https://example.com:${port}`)
 })
