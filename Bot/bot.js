64012b0b5f085844ca12ab947a8bc7ae9108fa26
magomedov025 <magomedov025@yahoo.com>
2024-07-17 19:58:48 +0300

    update link

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,10 +2,11 @@ const { Telegraf } = require('telegraf')
 const TOKEN = '7287053876:AAHA67oKvVDzNxPZZbsHgL873GyrmJA_Tg0'
 const bot = new Telegraf(TOKEN)
 
-const web_link = 'https://menybot.netlify.app/'
+// const web_link = 'https://menybot.netlify.app/'
+const web_link = 'https://www.profident05.ru/auth'
 
 bot.start(ctx =>
-	ctx.reply('Welcome :)))))', {
+	ctx.reply('Добро пожаловать)))', {
 		reply_markup: {
 			keyboard: [[{ text: 'web app', web_app: { url: web_link } }]],
 		},
